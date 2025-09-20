21e5dcd396d906d1cf537a9f88a53eee6da8410c
Plutzz <benjiongco@gmail.com>
2025-09-19 17:48:03 -0700

    Item Hold

--- CS4700SurvivalProject/Assets/_Scripts/Player/PlayerMove.cs
+++ CS4700SurvivalProject/Assets/_Scripts/Player/PlayerMove.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using MoreMountains.Feedbacks;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMove : State
 {
     [SerializeField] private Player player;
     [SerializeField] private AnimationClip up, right, left, down;
+    [SerializeField] private float itemBobSpeed;
+    [SerializeField] private float itemBobAmount;
+    [SerializeField] private Transform itemHold;
     private PlayerStats stats => player.stats;
     private PlayerInput input => player.playerInput;
     public override void DoUpdateState()
     {
         Vector2 moveInput = input.moveVector.normalized;
         rb.velocity = moveInput * stats.moveSpeed;
+        
+        // Item Bob
+        float newY = Mathf.Sin(Time.time * itemBobSpeed) * itemBobAmount;
+        itemHold.localPosition = new Vector3(0, newY, 0);
         
         if (Mathf.Abs(player.lookDir.x) > Mathf.Abs(player.lookDir.y))
         {
@@ -33,6 +42,7 @@ public class PlayerMove : State
 
     public override void DoExitState()
     {
-        
+        itemHold.localPosition = new Vector3(0, 0, 0);
+        base.DoExitState();
     }
 }
