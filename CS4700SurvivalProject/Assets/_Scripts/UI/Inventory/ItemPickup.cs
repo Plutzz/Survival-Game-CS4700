732e44aec58a5b904d55c79ea252f86b8af7c389
Plutzz <benjiongco@gmail.com>
2025-09-28 17:34:31 -0700

    Fix Item spawn

--- CS4700SurvivalProject/Assets/_Scripts/UI/Inventory/ItemPickup.cs
+++ CS4700SurvivalProject/Assets/_Scripts/UI/Inventory/ItemPickup.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class ItemPickup : MonoBehaviour
+public class ItemPickup : NetworkBehaviour
 {
     public Item itemDefinition; // Assign the ScriptableObject
     public string instanceId;
