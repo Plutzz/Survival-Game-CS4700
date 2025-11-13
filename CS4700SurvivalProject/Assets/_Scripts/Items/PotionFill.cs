e9ab7d62f666d69bf3edf53e2cc2dcba6f697ba8
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-11-12 22:18:28 -0800

    Able to fill potions

--- CS4700SurvivalProject/Assets/_Scripts/Items/PotionFill.cs
+++ CS4700SurvivalProject/Assets/_Scripts/Items/PotionFill.cs
@@ -4,5 +4,40 @@ using UnityEngine;
 
 public class PotionFill : MonoBehaviour
 {
-    //Replace empty bottle with filled potion
+    public bool isWater;
+    public bool isMud;
+    public bool isSaltWater;
+    public bool isMob;
+    
+    [Header("Potion References")]
+    public ItemSO waterPotion;
+    public ItemSO mudPotion;
+    public ItemSO saltWaterPotion;
+    public ItemSO mobPotion;
+    
+    void Update()
+    {
+        if (Interacting.interacted)
+        {
+            Debug.Log("Potion filled");
+            InventoryManager.Instance.RemoveSelectedItem();
+            
+            if (isWater && waterPotion != null)
+            {
+                InventoryManager.Instance.AddItem(waterPotion);
+            }
+            else if (isMud && mudPotion != null)
+            {
+                InventoryManager.Instance.AddItem(mudPotion);
+            }
+            else if (isSaltWater && saltWaterPotion != null)
+            {
+                InventoryManager.Instance.AddItem(saltWaterPotion);
+            }
+            else if (isMob && mobPotion != null)
+            {
+                InventoryManager.Instance.AddItem(mobPotion);
+            }
+        }
+    }
 }
